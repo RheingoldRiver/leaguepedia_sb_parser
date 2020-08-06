4c4e7e06fbcdfd9e8769c2b01d10d5040feb6e6a
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-08-05 23:56:42 -0500

    support include_header in multi_series parser

--- leaguepedia_sb_parser/multi_series_parser.py
+++ leaguepedia_sb_parser/multi_series_parser.py
@@ -12,11 +12,12 @@ class MultiParser(object):
             self.end = '\n{{box|end}}'
         self.parser = parser
     
-    def parse_multi_series(self, text):
+    def parse_multi_series(self, text, include_header=True):
         list_of_series = text.split('\n\n')
         output = []
         for series in list_of_series:
             series_array = series.split('\n')
-            output.append(self.parser.parse_series(series_array))
+            output.append(self.parser.parse_series(series_array,
+                                                   include_header=include_header))
         
         return self.start + self.between.join(output) + self.end
