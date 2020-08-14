f6493a5ace0cf2c48178c8c8ae2a60dd86da8f53
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-08-14 14:59:45 -0500

    more error msg fix

--- leaguepedia_sb_parser/parser.py
+++ leaguepedia_sb_parser/parser.py
@@ -134,7 +134,7 @@ class Parser(object):
     def get_resolved_patch(self, patch):
         if self.patch != patch:
             self.warnings.append(
-                'Provided patch doesn\'t match ingame patch!! Provided: {}, ingame {}. We used ingame but please fix the wiki!'.format(
+                'MatchSchedule patch doesn\'t match actual (match history) patch!! MS: {}, ingame {}. We used mh but please fix the wiki!'.format(
                     self.patch, patch))
         return patch
     
