b26ea9554987071ce75c8a2d2672d1d3d97f3585
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-03-30 12:07:17 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="leaguepedia_sb_parser",
-    version="0.0.2",
+    version="0.0.3",
     author="RheingoldRiver",
     author_email="river.esports@gmail.com",
     description="Parser for Leaguepedia scoreboards",
