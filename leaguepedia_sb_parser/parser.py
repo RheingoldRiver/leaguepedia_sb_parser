b14633ad3a616146061f88e2112b8aa1f614f42b
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-07-23 15:03:16 -0500

    log event too

--- leaguepedia_sb_parser/parser.py
+++ leaguepedia_sb_parser/parser.py
@@ -109,8 +109,8 @@ class Parser(object):
             self.warnings.append(
                 'Final team name for {} is missing (original: `{}`) - maybe you have to set |short= in TeamRoster if the \
                  code differs from what we use in Teamnames. You may also need to RefreshTeamnames or blank edit \
-                the team page'.format(
-                    team_key, team_name
+                the team page. Inferred event: {}'.format(
+                    team_key, team_name, self.event
                 )
             )
         return result
